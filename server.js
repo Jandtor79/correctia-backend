cbd8e629581694baf28536a75f021384d038cbfa
Jandtor79 <jonathan_andrades@hotmail.com>
2026-04-02 13:24:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.post("/audio", upload.single("audio"), async (req, res) => {
 });
 
 // 🚀 ARRANQUE
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor activo en puerto", PORT);
